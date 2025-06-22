d65972f67b6a58b442519b0846ca5527d1e3da9f
annanasnas <avsyurakshina@miem.hse.ru>
2025-06-22 14:53:07 +0300

    add weights

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,18 +1,24 @@
 import argparse, pathlib, zipfile, gdown
 
-FILE_ID = "1OlElYRhKovWEc8gu32E8at-xNfEQniGq"
-URL     = f"https://drive.google.com/uc?id={FILE_ID}"
+CITYSCAPES_ID = "1OlElYRhKovWEc8gu32E8at-xNfEQniGq"
+WEIGHTS_ID = "1VOmwEwd73ktbCaSlugCPRGEwq9QY85aW"
+
+def gdrive(id_: str, out: pathlib.Path):
+    url = f"https://drive.google.com/uc?id={id_}"
+    gdown.download(url, str(out), quiet=False)
 
 def main(dest: str) -> None:
     dest = pathlib.Path(dest).expanduser()
     dest.mkdir(parents=True, exist_ok=True)
 
     zip_path = dest / "Cityscapes.zip"
-    gdown.download(URL, str(zip_path), quiet=False)
-
-    with zipfile.ZipFile(zip_path) as zf:
-        zf.extractall(dest)
+    gdrive(CITYSCAPES_ID, zip_path)
+    with zipfile.ZipFile(zip_path) as zf: zf.extractall(dest)
     zip_path.unlink()
+
+    ckpt_path = dest / "deeplabv2_cityscapes.pt"
+    if not ckpt_path.exists():
+        gdrive(WEIGHTS_ID, ckpt_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
