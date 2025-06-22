62fd725c9fe529a2827e13443eaa71edbcbd7344
annanasnas <avsyurakshina@miem.hse.ru>
2025-06-22 15:16:14 +0300

    change routs

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,26 +1,24 @@
-import argparse, pathlib, zipfile, gdown
+import pathlib, zipfile, gdown
 
 CITYSCAPES_ID = "1OlElYRhKovWEc8gu32E8at-xNfEQniGq"
 WEIGHTS_ID = "1VOmwEwd73ktbCaSlugCPRGEwq9QY85aW"
 
-def gdrive(id_: str, out: pathlib.Path):
+def gdrive(id_: str, out_path: pathlib.Path):
     url = f"https://drive.google.com/uc?id={id_}"
-    gdown.download(url, str(out), quiet=False)
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+    gdown.download(url, str(out_path), quiet=False)
 
-def main(dest: str) -> None:
-    dest = pathlib.Path(dest).expanduser()
-    dest.mkdir(parents=True, exist_ok=True)
-
-    zip_path = dest / "Cityscapes.zip"
+def main():
+    
+    zip_path = pathlib.Path("datasets/data/Cityscapes.zip")
     gdrive(CITYSCAPES_ID, zip_path)
-    with zipfile.ZipFile(zip_path) as zf: zf.extractall(dest)
+    with zipfile.ZipFile(zip_path) as zf:
+        zf.extractall(zip_path.parent)
     zip_path.unlink()
 
-    weights_path = "models/deeplabv2/deeplabv2_cityscapes.pt"
+    weights_path = pathlib.Path("models/deeplabv2/DeepLab_resnet_pretrained_imagenet.pt")
     if not weights_path.exists():
         gdrive(WEIGHTS_ID, weights_path)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    main(args.dest)
+    main()
