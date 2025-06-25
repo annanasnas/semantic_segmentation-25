287c3b1e9ea0efaad1b8e2315ba6694feaaa62d0
annanasnas <avsyurakshina@miem.hse.ru>
2025-06-25 05:48:13 +0300

    add FDA

--- datasets/gta5.py
+++ datasets/gta5.py
@@ -6,12 +6,15 @@ import torch
 from torchvision import transforms
 import numpy as np
 import albumentations as A
+from torch.fft import fft2, ifft2
+import random
 
 
 class GTA5Dataset(Dataset):
-    def __init__(self, root_dir, image_transform=None, image_size=(720, 1280)):
+    def __init__(self, root_dir, image_transform=None, image_size=(720, 1280), FDA=False, beta=0.01):
 
         self.root_dir = root_dir
+        self.cityscapes_image_paths = sorted(glob.glob(os.path.join(os.path.join('/content/semantic_segmentation-25/datasets/data/Cityscapes/Cityspaces', 'images', 'train'), '**', '*_leftImg8bit.png'), recursive=True))
 
         self.images_dir = os.path.join(root_dir, 'images')
         self.labels_dir = os.path.join(root_dir, 'labels')
@@ -20,7 +23,15 @@ class GTA5Dataset(Dataset):
         self.label_paths = sorted(glob.glob(os.path.join(self.labels_dir, '*.png')))
 
         self.image_transform = image_transform
+        self.image_target_transform = transforms.Compose([
+                                                              transforms.ToTensor(),
+                                                              transforms.Normalize(mean=imagenet_mean, std=imagenet_std)
+                                                          ])
         self.image_size = image_size
+
+        self.FDA = FDA
+        self.beta = beta
+        self.to_tensor = transforms.ToTensor()
 
         assert len(self.image_paths) == len(self.label_paths), "Mismatch between image and label counts"
 
@@ -43,6 +54,28 @@ class GTA5Dataset(Dataset):
         label_np = np.array(label, dtype=np.uint8)
         label_remapped = self.remap_gta5_labels(label_np)
         label = torch.as_tensor(label_remapped, dtype=torch.long)
+
+        if self.FDA:
+            trg_path = random.choice(self.cityscapes_image_paths)
+            trg_image = Image.open(trg_path).convert("RGB")
+            trg_image = transforms.Resize(self.image_size)(trg_image)
+
+            src_tensor = self.to_tensor(image).unsqueeze(0)
+            trg_tensor = self.to_tensor(trg_image).unsqueeze(0)
+
+            with torch.no_grad():
+                fda_img_tensor = self.FDA_source_to_target(src_tensor, trg_tensor, beta=self.beta)[0]
+            
+            fda_np = fda_img_tensor.permute(1, 2, 0).cpu().numpy()
+            fda_np = (fda_np * 255).clip(0, 255).astype(np.uint8)
+
+            transformed = self.image_transform(image=fda_np, mask=label_remapped)
+            image = transformed['image']
+            label = transformed['mask'].long()
+
+            trg_image = self.image_target_transform(trg_image)
+
+            return image, label, trg_image
 
         # albumentations
         if isinstance(self.image_transform, A.Compose):
@@ -85,4 +118,36 @@ class GTA5Dataset(Dataset):
             remapped[label_mask == gta_label] = cityscapes_label
 
         return remapped
+
+
+    def low_freq_mutate(self, amp_s, amp_t, beta):
+
+        a_s = torch.fft.fftshift(amp_s, dim=(-2,-1))
+        a_t = torch.fft.fftshift(amp_t, dim=(-2,-1))
+
+        B,C,H,W = a_s.shape
+        b_h = int(H * beta/2)
+        b_w = int(W * beta/2)
+        h1, h2 = H//2 - b_h, H//2 + b_h
+        w1, w2 = W//2 - b_w, W//2 + b_w
+
+        a_s[:, :, h1:h2, w1:w2] = a_t[:, :, h1:h2, w1:w2]
+
+        return torch.fft.ifftshift(a_s, dim=(-2,-1))
+
+
+    def FDA_source_to_target(self, x_s, x_t, beta):
+        # 1. forward 2D FFT
+        F_s = fft2(x_s, dim=(-2, -1))
+        F_t = fft2(x_t, dim=(-2, -1))
+        # 2. split amplitude & phase
+        A_s, P_s = torch.abs(F_s), torch.angle(F_s)
+        A_t = torch.abs(F_t)
+        # 3. swap low-frequencies
+        A_mix = self.low_freq_mutate(A_s.clone(), A_t.clone(), beta)
+        # 4. reassemble complex spectrum
+        F_mix = A_mix * torch.exp(1j * P_s)
+        # 5. inverse FFT back to image
+        x_s_to_t = ifft2(F_mix, dim=(-2, -1)).real
+        return x_s_to_t
         
--- scripts/train.py
+++ scripts/train.py
@@ -6,6 +6,7 @@ from scripts.utils import fast_hist, per_class_iou, denormalize, poly_lr_schedul
 from pathlib import Path
 import matplotlib.pyplot as plt
 from IPython.display import display
+import torch.nn.functional as F
 
 
 CITYSCAPES_CLASSES = [
@@ -22,12 +23,52 @@ CITYSCAPES_PALETTE = [
 ]
 
 
-def train_model(model, train_dataloader, val_dataloader,
-                device, epochs, autocast, scaler,
-                optimizer, criterion, learning_rate,
-                iteration, max_iter,
-                ckpt, start_epoch, best_miou,
-                log_csv, metrics):
+def validate(model, autocast, val_dataloader, device, epoch, epochs, 
+            train_loss, metrics, log_csv):
+    model.eval()
+    hist = np.zeros((19, 19))
+
+    with torch.no_grad():
+        for images, masks in tqdm(val_dataloader, desc=f"Epoch {epoch+1}/{epochs} [Val]", leave=False):
+            images, masks = images.to(device), masks.to(device, dtype=torch.long)
+
+            with autocast(device_type='cuda'):
+                outputs = model(images)
+
+            preds = torch.argmax(outputs, dim=1).cpu().numpy()
+            targets = masks.cpu().numpy()
+            targets[targets == 255] = -1
+            for lt, lp in zip(targets, preds):
+                hist += fast_hist(lt.flatten(), lp.flatten(), 19)
+    
+    ious = per_class_iou(hist)
+    miou = np.nanmean(ious)
+    print("Per-class IoU")
+    for idx, (cls_name, iou) in enumerate(zip(CITYSCAPES_CLASSES, ious)):
+        print(f"{cls_name:16s}: {iou:.4f}")
+    print(f"Mean IoU: {np.nanmean(ious):.4f}")
+
+    new_row = {
+        "epoch":      epoch + 1,
+        "train_loss": train_loss,
+        "val_miou":   miou,
+    }
+    metrics["epoch"].append(new_row["epoch"])
+    metrics["train_loss"].append(new_row["train_loss"])
+    metrics["val_miou"].append(new_row["val_miou"])
+    pd.DataFrame([new_row]).to_csv(
+        log_csv, mode="a", index=False, header=not log_csv.exists()
+    )
+
+    return miou, metrics
+
+
+def train_deeplab(model, train_dataloader, val_dataloader,
+                  device, epochs, autocast, scaler,
+                  optimizer, criterion, learning_rate,
+                  iteration, max_iter,
+                  ckpt, start_epoch, best_miou,
+                  log_csv, metrics):
     
 
     log_csv = Path(log_csv)
@@ -65,40 +106,9 @@ def train_model(model, train_dataloader, val_dataloader,
         train_loss = running_loss / len(train_dataloader)
         print(f"Epoch {epoch+1}/{epochs}, Loss: {train_loss:.4f}")
 
-        model.eval()
-        hist = np.zeros((19, 19))
-
-        with torch.no_grad():
-            for images, masks in tqdm(val_dataloader, desc=f"Epoch {epoch+1}/{epochs} [Val]", leave=False):
-                images, masks = images.to(device), masks.to(device, dtype=torch.long)
-
-                with autocast(device_type='cuda'):
-                    outputs = model(images)
-
-                preds = torch.argmax(outputs, dim=1).cpu().numpy()
-                targets = masks.cpu().numpy()
-                targets[targets == 255] = -1
-                for lt, lp in zip(targets, preds):
-                    hist += fast_hist(lt.flatten(), lp.flatten(), 19)
-        
-        ious = per_class_iou(hist)
-        miou = np.nanmean(ious)
-        print("Per-class IoU")
-        for idx, (cls_name, iou) in enumerate(zip(CITYSCAPES_CLASSES, ious)):
-            print(f"{cls_name:16s}: {iou:.4f}")
-        print(f"Mean IoU: {np.nanmean(ious):.4f}")
-
-        new_row = {
-            "epoch":      epoch + 1,
-            "train_loss": train_loss,
-            "val_miou":   miou,
-        }
-        metrics["epoch"].append(new_row["epoch"])
-        metrics["train_loss"].append(new_row["train_loss"])
-        metrics["val_miou"].append(new_row["val_miou"])
-        pd.DataFrame([new_row]).to_csv(
-            log_csv, mode="a", index=False, header=not log_csv.exists()
-        )
+        miou, metrics = validate(model, autocast, val_dataloader, 
+				device, epoch, epochs, 
+				train_loss, metrics, log_csv)
 
         line_train.set_data(metrics["epoch"], metrics["train_loss"])
         line_miou.set_data(metrics["epoch"], metrics["val_miou"])
@@ -126,4 +136,174 @@ def train_model(model, train_dataloader, val_dataloader,
 
     ckpt.save_final(snapshot, epochs)
 
+
+
+def train_bisenet(model, train_dataloader, val_dataloader,
+                device, epochs, autocast, scaler,
+                optimizer, criterion, learning_rate,
+                iteration, max_iter,
+                ckpt, start_epoch, best_miou,
+                log_csv, metrics):
+    
+
+    log_csv = Path(log_csv)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    line_train, = ax1.plot([], [], label="train_loss")
+    ax1.set_xlabel("epoch"); ax1.set_ylabel("loss"); ax1.grid(); ax1.legend()
+    line_miou,  = ax2.plot([], [], color="green", label="val_miou")
+    ax2.set_xlabel("epoch"); ax2.set_ylabel("mIoU"); ax2.grid()
+    fig.suptitle("Training progress"); fig.tight_layout()
+    plot_handle = display(fig, display_id="loss_miou")
+
+    for epoch in range(start_epoch, epochs):
+
+        model.train()
+        running_loss = 0.0
+
+        for images, masks in tqdm(train_dataloader, desc=f"Epoch {epoch+1}/{epochs} [Train]", leave=False):
+            images, masks = images.to(device), masks.to(device, dtype=torch.long)
+            optimizer.zero_grad()
+
+            with autocast(device_type='cuda'):
+                outputs, aux1, aux2 = model(images)
+                loss_main = criterion(outputs, masks)
+                loss_aux1 = criterion(aux1, masks)
+                loss_aux2 = criterion(aux2, masks)
+                loss = loss_main + 0.4 * loss_aux1 + 0.4 * loss_aux2
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            running_loss += loss.item()
+
+            poly_lr_scheduler(optimizer, learning_rate, iteration, max_iter=max_iter)
+            iteration += 1
+
+        train_loss = running_loss / len(train_dataloader)
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {train_loss:.4f}")
+
+        miou, metrics = validate(model, autocast, val_dataloader, 
+				device, epoch, epochs, 
+				train_loss, metrics, log_csv)
+
+        line_train.set_data(metrics["epoch"], metrics["train_loss"])
+        line_miou.set_data(metrics["epoch"], metrics["val_miou"])
+
+        for ax in (ax1, ax2):
+            ax.relim()
+            ax.autoscale_view()
+
+        plot_handle.update(fig)
+
+        snapshot = {
+              "epoch":      epoch + 1,
+              "model":      model.cpu().state_dict(),
+              "optimizer":  optimizer.state_dict(),
+              "scaler":     scaler.state_dict(),
+              "iteration":  iteration,
+              "best_miou":  best_miou
+          }
+        ckpt.save_last(snapshot)
+        model.to(device)
+
+        if miou > best_miou:
+            best_miou = miou
+            ckpt.save_best(miou, snapshot)
+
+    ckpt.save_final(snapshot, epochs)
+
+
+def train_bisenet_FDA(model, train_dataloader, val_dataloader,
+                      device, epochs, autocast, scaler,
+                      optimizer, criterion, learning_rate,
+                      iteration, max_iter,
+                      ckpt, start_epoch, best_miou,
+                      log_csv, metrics,
+                      charbonnier_eps, charbonnier_alpha, lambda_ent):
+    
+
+    log_csv = Path(log_csv)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    line_train, = ax1.plot([], [], label="train_loss")
+    ax1.set_xlabel("epoch"); ax1.set_ylabel("loss"); ax1.grid(); ax1.legend()
+    line_miou,  = ax2.plot([], [], color="green", label="val_miou")
+    ax2.set_xlabel("epoch"); ax2.set_ylabel("mIoU"); ax2.grid()
+    fig.suptitle("Training progress"); fig.tight_layout()
+    plot_handle = display(fig, display_id="loss_miou")
+
+    for epoch in range(start_epoch, epochs):
+
+        model.train()
+        running_loss = 0.0
+        total_ce = 0.0
+        total_ent = 0.0
+
+        for images, masks, target_images in tqdm(train_dataloader, desc=f"Epoch {epoch+1}/{epochs} [Train]", leave=False):
+            images, masks = images.to(device), masks.to(device, dtype=torch.long)
+            target_images = target_images.to(device)
+            optimizer.zero_grad()
+
+            with autocast(device_type='cuda'):
+                outputs, aux1, aux2 = model(images)
+                loss_main = criterion(outputs, masks)
+                loss_aux1 = criterion(aux1, masks)
+                loss_aux2 = criterion(aux2, masks)
+                loss_ce = loss_main + 0.4 * loss_aux1 + 0.4 * loss_aux2
+
+                logits_t = model(target_images)[0]
+                probs_t = F.softmax(logits_t, dim=1)
+                safe_probs = probs_t.clamp(min=1e-8)
+                H = -torch.sum(safe_probs * torch.log(safe_probs), dim=1)
+                loss_ent = torch.mean((H**2 + charbonnier_eps**2)**charbonnier_alpha)
+                loss = loss_ce + lambda_ent * loss_ent
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            running_loss += loss.item()
+            total_ce += loss_ce.item()
+            total_ent += loss_ent.item()
+
+            poly_lr_scheduler(optimizer, learning_rate, iteration, max_iter=max_iter)
+            iteration += 1
+
+        train_loss = running_loss / len(train_dataloader)
+        avg_ce = total_ce / len(train_dataloader)
+        avg_ent = total_ent / len(train_dataloader)
+
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss / len(train_dataloader):.4f}, — CE: {avg_ce:.4f}  Ent: {avg_ent:.4f}")
+
+        miou, metrics = validate(model, autocast, val_dataloader, 
+				device, epoch, epochs, 
+				train_loss, metrics, log_csv)
+
+        line_train.set_data(metrics["epoch"], metrics["train_loss"])
+        line_miou.set_data(metrics["epoch"], metrics["val_miou"])
+
+        for ax in (ax1, ax2):
+            ax.relim()
+            ax.autoscale_view()
+
+        plot_handle.update(fig)
+
+        snapshot = {
+              "epoch":      epoch + 1,
+              "model":      model.cpu().state_dict(),
+              "optimizer":  optimizer.state_dict(),
+              "scaler":     scaler.state_dict(),
+              "iteration":  iteration,
+              "best_miou":  best_miou
+          }
+        ckpt.save_last(snapshot)
+        model.to(device)
+
+        if miou > best_miou:
+            best_miou = miou
+            ckpt.save_best(miou, snapshot)
+
+    ckpt.save_final(snapshot, epochs)
 
